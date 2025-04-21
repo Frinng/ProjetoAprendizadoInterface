b1c311288d8fb236aa5f6cc48ecec2e9508a3321
Frinng <167120714+Frinng@users.noreply.github.com>
2025-04-20 22:23:47 -0300

    projeto 1.1

--- src/principal.java
+++ src/principal.java
@@ -1,9 +1,12 @@
-import javax.swing.*; //esta biblioteca serve para criar interface grafica
-import java.awt.event.ActionEvent; //esta biblioteca serve para responder o usuario
-import java.awt.event.ActionListener;//esta biblioteca serve para receber os dados do usuario
+import javax.swing.*; //esta biblioteca serve para criar interface grafica //Criar os elementos da interface gráfica
+import java.awt.event.ActionEvent; //esta biblioteca serve para responder o usuario //	Representa o evento (clique, etc.)
+import java.awt.event.ActionListener;//esta biblioteca serve para receber os dados do usuario//Permite programar a reação ao evento
 
 public class principal {
     public static void main(String[] args) {
 
+        JFrame tela = new JFrame("Teste de Interface");
+        tela.setSize(300,200);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
