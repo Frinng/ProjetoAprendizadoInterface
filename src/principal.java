36baa424e5959db361343131d2a2ead816eb26ab
Frinng <167120714+Frinng@users.noreply.github.com>
2025-04-20 22:18:35 -0300

    projeto 1.0

---
+++ src/principal.java
@@ -0,0 +1,9 @@
+import javax.swing.*; //esta biblioteca serve para criar interface grafica
+import java.awt.event.ActionEvent; //esta biblioteca serve para responder o usuario
+import java.awt.event.ActionListener;//esta biblioteca serve para receber os dados do usuario
+
+public class principal {
+    public static void main(String[] args) {
+
+    }
+}
